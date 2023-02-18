671edbc632843b926177bf886cff014834889f86
Jacob Bailey <jayembailey@gmail.com>
2023-02-18 13:13:11 -0500

    fixed signin/up thing

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
-
 class Config():
     '''
         Set config variables for the flask app using 
@@ -12,6 +11,7 @@ class Config():
 
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
-    SECRET_KEY = os.environ.get('4567_superJuice')
+    SECRET_KEY = '4567_superJuice'
+    WTF_CSRF_SECRET_KEY = 'SECRET_SECRET_SECRET'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
